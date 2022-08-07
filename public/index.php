516ac412850451b291ac680a66154c0f310123b8
Marcus Thulio <marcusthuliok9@gmail.com>
2022-08-07 06:10:52 -0300

    Aplicando o modelo e layout basico

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory Management</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<!-- Compiled and minified JavaScript -->
+
+</head>
+<body>
+
+<div id="container">
+<p> Selecione a opção de gerenciamento. </p>
+    <div id="panel">
+        <a class="waves-effect waves-light btn" id="button-product">Produtos</a>
+        <a class="waves-effect waves-light btn" id="button-group">Grupos</a>
+    </div>
+</div>
+
+
+
+
+
+
+<script src="js/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+</html>
